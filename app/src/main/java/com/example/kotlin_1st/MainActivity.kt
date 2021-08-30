b7ed43f57416162496a944e0c76edced94ab9f54
jg20228 <jg20228@nate.com>
2021-08-30 23:27:45 +0900

    가로로 쌓이는 LinearLayout

--- app/src/main/java/com/example/kotlin_1st/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_1st/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotlin_1st
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        Log.d("BasicSyntax","로그를 출력합니다. method = Log.d")
+
+        var name = "홍길동"
+        Log.d("BasicSyntax", "제 이름은 $name 입니다.") //$name은 코드로 인식된다.    }
 }
